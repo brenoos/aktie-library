c36b10072491c6493d4a318439d830de04886be7
Breno Oliveira dos Santos <breno.oliver07@gmail.com>
2018-08-22 00:49:48 -0300

    action a fazer

--- frontend/src/avaliacao/avaliacaoActions.js
+++ frontend/src/avaliacao/avaliacaoActions.js
@@ -3,7 +3,17 @@ import { toastr }  from 'react-redux-toastr'
 import consts from '../consts'
 
 export const validaLocador = locador => {
-
+    return dispatch => {
+        const id = values._id ? values._id : ''
+        axios[method](`${BASE_URL}/billingCycles/${id}`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+                dispatch()
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error))
+            })
+    }
 }
 
 
